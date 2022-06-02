ac9fb8832eb6450c8b69f35d0e2ba0bf08dac4f5
tungsfer512 <tungbv5122001@gmail.com>
2022-06-02 09:20:01 +0700

    index

--- js/index.js
+++ js/index.js
@@ -14,4 +14,21 @@ $(document).ready(function () {
     prevArrow: '<button type="button" class="slick-prev"><span class="material-icons">arrow_back</span></button>',
     nextArrow: '<button type="button" class="slick-next"><span class="material-icons">arrow_forward</span></i></button>'
   });
+  $('.bestsell-container-content').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    dots: false,
+    infinite: true,
+    speed: 500,
+    // fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: true, 
+    prevArrow: '<button type="button" class="slick-prev"><span class="material-icons">arrow_back</span></button>',
+    nextArrow: '<button type="button" class="slick-next"><span class="material-icons">arrow_forward</span></i></button>'
+  });
+
+  $(".bestsell .slick-arrow").addClass("btn-hover-primary");
+
 });
